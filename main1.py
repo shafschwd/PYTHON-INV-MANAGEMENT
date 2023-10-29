6f78e37575e3aedb3f610880e2f65ea2fddef942
shafs <35454710+shafschwd@users.noreply.github.com>
2023-10-29 20:01:42 +0800

    fixed User management system

--- main1.py
+++ main1.py
@@ -11,6 +11,7 @@ def read_user_data():
             }
     return user_data
 
+
 # Function to write user data back to the users.txt file
 def write_user_data(user_data):
     with open("users.txt", "w") as file:
@@ -20,11 +21,13 @@ def write_user_data(user_data):
             user_type = details['user_type']
             file.write(f"{user_id},{username},{password},{user_type}\n")
 
+
 # Function to authenticate a user
 def authenticate_user(user_data, username, password):
     if username in user_data and user_data[username]['password'] == password:
         return user_data[username]
     return None
+
 
 # Function for admin to create a new user
 def create_user(user_data):
@@ -44,22 +47,34 @@ def create_user(user_data):
     }
     print("User created successfully!")
 
+
 # Function for admin to modify a user's details
 def modify_user(user_data):
     username = input("Enter the username to modify: ")
     if username in user_data:
-        password = input("Enter the new password: ")
+        user = user_data[username]
+        password = input("Enter the new password (leave blank to keep the current password): ")
         user_type = input("Enter the new user type (admin or staff): ")
+        new_username = input("Enter the new username (leave blank to keep the current username): ")
 
         if user_type not in ['admin', 'staff']:
             print("Invalid user type. User not modified.")
             return
 
-        user_data[username]['password'] = password
-        user_data[username]['user_type'] = user_type
-        print("User details modified successfully!")
+        if password:
+            user['password'] = password
+        if user_type:
+            user['user_type'] = user_type
+        if new_username and new_username != username:
+            user_data[new_username] = user
+            del user_data[username]
+
+        write_user_data(user_data)
+        print(f"User details modified successfully.")
     else:
         print("User not found.")
+
+
 
 # Function for admin to search for a user
 def search_user(user_data):
@@ -70,11 +85,28 @@ def search_user(user_data):
     else:
         print("User not found.")
 
-# Main program
+# Function to delete a user
+def delete_user(user_data, username):
+    if username in user_data:
+        del user_data[username]
+        write_user_data(user_data)
+        print(f"{username} has been deleted.")
+    else:
+        print("User not found.")
+# Function to display all users
+def display_users():
+    with open("users.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            user_id, username, _, user_type = line.strip().split(',')
+            print(f"User ID: {user_id}, Username: {username}, User Type: {user_type}")
+
+
+# User management system
 if __name__ == "__main__":
     user_data = read_user_data()
 
-    while True:
+    while True:  # This outer loop keeps the program running
         print("\nLogin System")
         username = input("Enter your username: ")
         password = input("Enter your password: ")
@@ -83,16 +115,24 @@ if __name__ == "__main__":
         if user:
             print(f"Welcome, {username} ({user['user_type']})!")
 
-            if user['user_type'] == 'admin':
-                while True:
+            while True:  # This inner loop is for the admin or staff menu
+                if user['user_type'] == 'admin':
                     print("\nAdmin Menu:")
                     print("1. Create User")
                     print("2. Modify User")
                     print("3. Search User")
-                    print("4. Logout")
+                    print("4. Delete Users")
+                    print("5. Display All Users")
+                    print("6. Logout")
+                else:
+                    print("\nStaff Menu:")
+                    print("1. Search User")
+                    print("2. Display All Users")
+                    print("3. Logout")
 
-                    choice = input("Enter your choice: ")
+                choice = input("Enter your choice: ")
 
+                if user['user_type'] == 'admin':
                     if choice == "1":
                         create_user(user_data)
                     elif choice == "2":
@@ -100,13 +140,31 @@ if __name__ == "__main__":
                     elif choice == "3":
                         search_user(user_data)
                     elif choice == "4":
+                        username = input("Enter the username to delete: ")
+                        delete_user(user_data, username)
+                    elif choice == "5":
+                        display_users()
+                    elif choice == "6":
                         write_user_data(user_data)
                         print("Logged out.")
                         break
                     else:
                         print("Invalid choice. Please try again.")
-            else:
-                print("You do not have admin privileges.")
+                else:
+                    if choice == "1":
+                        search_user(user_data)
+                    elif choice == "2":
+                        display_users()
+                    elif choice == "3":
+                        write_user_data(user_data)
+                        print("Logged out.")
+                        break
+                    else:
+                        print("Invalid choice. Please try again.")
+
+            user_continue = input("Do you want to continue (Y/N)? ").strip().lower()
+            if user_continue != 'y':
                 break
         else:
             print("Invalid username or password. Please try again.")
+
